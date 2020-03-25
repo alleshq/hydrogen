ba60fb91695365ec3ab64057e67d3c49e6adde8c
Archie Baer <archie@abaer.dev>
2020-03-25 01:43:56 +0000

    TitleBar has buttons and user image

--- src/layout/TitleBar.js
+++ src/layout/TitleBar.js
@@ -2,6 +2,24 @@ import React from "react";
 
 export default () => (
 	<div className="titleBar">
-		<img className="logo" src="h10.svg" />
+		<div className="left">
+			<img className="logo" src="h10.svg" />
+		</div>
+		<div className="right">
+			<img
+				className="user"
+				draggable="false"
+				src="https://avatar.alles.cx/u/archie"
+			/>
+			<div className="minimize">
+				<i className="material-icons">minimize</i>
+			</div>
+			<div className="maximize">
+				<i className="material-icons">fullscreen</i>
+			</div>
+			<div className="close">
+				<i className="material-icons">close</i>
+			</div>
+		</div>
 	</div>
 );
