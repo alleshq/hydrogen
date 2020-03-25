14c3d24205b1110b70f9f72f6ee8cacbf1c503fa
Archie Baer <archie@abaer.dev>
2020-03-25 11:03:00 +0000

    TitleBar buttons function

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, ipcMain} = require("electron");
 const isDev = require("electron-is-dev");
 app.allowRendererProcessReuse = true;
 
+//Create Window
 const createWindow = () => {
-	const mainWindow = new BrowserWindow({
+	const win = new BrowserWindow({
 		width: 1000,
 		minWidth: 1000,
 		height: 700,
 		minHeight: 700,
 		frame: false,
 		webPreferences: {
-			preload: __dirname + "/preload.js",
-		},
+			preload: __dirname + "/appPreload.js"
+		}
 	});
 
-	mainWindow.loadURL(
+	//Load App Page
+	win.loadURL(
 		isDev ? "http://localhost:3000" : `file://${__dirname}/../build/index.html`
 	);
 };
 app.whenReady().then(createWindow);
 
+//End process on windows closed
 app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") app.quit();
 });
 
+//macOS activate
 app.on("activate", () => {
 	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+//IPC
+ipcMain.on("asynchronous-message", (event, ...args) => {
+	const win = BrowserWindow.getFocusedWindow();
+
+	if (args[0] === "app.minimize" && win) {
+		win.minimize();	
+	} else if (args[0] === "app.maximize" && win) {
+		if (win.isMaximized()) {
+			win.unmaximize();
+		} else {
+			win.maximize();
+		}
+	}
 });
--- src/layout/TitleBar.js
+++ src/layout/TitleBar.js
@@ -3,21 +3,22 @@ import React from "react";
 export default () => (
 	<div className="titleBar">
 		<div className="left">
-			<img className="logo" src="h10.svg" />
+			<img className="logo" alt="" src="h10.svg" />
 		</div>
 		<div className="right">
 			<img
 				className="user"
 				draggable="false"
+				alt=""
 				src="https://avatar.alles.cx/u/archie"
 			/>
-			<div className="minimize">
+			<div className="minimize" onClick={window.app.minimize}>
 				<i className="material-icons">minimize</i>
 			</div>
-			<div className="maximize">
+			<div className="maximize" onClick={window.app.maximize}>
 				<i className="material-icons">fullscreen</i>
 			</div>
-			<div className="close">
+			<div className="close" onClick={window.close}>
 				<i className="material-icons">close</i>
 			</div>
 		</div>
