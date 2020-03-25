f126cb671531002266446109cf2d1ef9418cdc64
Archie Baer <archie@abaer.dev>
2020-03-25 14:43:13 +0000

    Formatting

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default () => (
 	<div className="navBar">
-        <div className="navButtons">
-            <i className="back material-icons">arrow_back_ios</i>
-            <i className="forward material-icons">arrow_forward_ios</i>
-            <i className="refresh material-icons active">refresh</i>
-        </div>
+		<div className="navButtons">
+			<i className="back material-icons">arrow_back_ios</i>
+			<i className="forward material-icons">arrow_forward_ios</i>
+			<i className="refresh material-icons active">refresh</i>
+		</div>
 	</div>
 );
