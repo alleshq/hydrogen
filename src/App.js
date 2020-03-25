9ae2957ff0b5abd23b0021f6e52e68c0906f59d4
Archie Baer <archie@abaer.dev>
2020-03-25 01:14:08 +0000

    Title Bar with Hydrogen Logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./style/index.scss";
 
+import TitleBar from "./layout/TitleBar";
+
 export default () => {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<TitleBar />
+		</div>
+	);
 };
---
+++ src/layout/TitleBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => (
+	<div className="titleBar">
+		<img className="logo" src="h10.svg" />
+	</div>
+);
