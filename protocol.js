8581c6204a85e195f4c9e21e42de0c8bdc901a4b
Archie Baer <archie@abaer.dev>
2020-03-30 01:48:01 +0100

    Formatting

--- protocol.js
+++ protocol.js
@@ -19,13 +19,13 @@ module.exports = () => {
 	session.fromPartition("tabs").protocol.registerHttpProtocol(
 		"hydrogen",
 		(request, callback) => {
-            var url = request.url.substr(11);
-            if (url.split("/")[1] === "static" || url.endsWith(".js")) {
-                url = url.split("/");
-                url.shift();
-                url = url.join("/");
+			var url = request.url.substr(11);
+			if (url.split("/")[1] === "static" || url.endsWith(".js")) {
+				url = url.split("/");
+				url.shift();
+				url = url.join("/");
 			}
-            callback({
+			callback({
 				url: isDev
 					? "http://localhost:5165/" + url
 					: `file://${__dirname}/internal/build/index.html`
