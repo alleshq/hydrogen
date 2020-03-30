88edc2ed23d9fb567f9da60bf193cc1656ad6bbe
Archie Baer <archie@abaer.dev>
2020-03-30 14:27:21 +0100

    Axios requests internal

--- protocol.js
+++ protocol.js
@@ -1,5 +1,6 @@
 const {protocol, session} = require("electron");
 const isDev = require("electron-is-dev");
+const axios = require("axios");
 
 protocol.registerSchemesAsPrivileged([
 	{
@@ -16,20 +17,27 @@ protocol.registerSchemesAsPrivileged([
 ]);
 
 module.exports = () => {
-	session.fromPartition("tabs").protocol.registerHttpProtocol(
+	session.fromPartition("tabs").protocol.registerStringProtocol(
 		"hydrogen",
-		(request, callback) => {
+		async (request, callback) => {
 			var url = request.url.substr(11);
 			if (url.split("/")[1] === "static" || url.endsWith(".js")) {
 				url = url.split("/");
 				url.shift();
 				url = url.join("/");
 			}
-			callback({
-				url: isDev
-					? "https://easylist.to/easylist/easylist.txt"
-					: `file://${__dirname}/internal/build/index.html`
-			});
+
+			const sourceUrl = isDev
+				? `http://localhost:5165/home`
+				: `file://${__dirname}/internal/build/index.html`;
+			
+			try {
+				const {data} = await axios.get(sourceUrl);
+				callback(data);
+			} catch (err) {
+				console.log(err.response.data);
+				callback("Failed to access internal pages");
+			}
 		},
 		error => {
 			if (error) console.error(error);
